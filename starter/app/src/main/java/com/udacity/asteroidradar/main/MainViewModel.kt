96c4c490ec9e989c94fff5bd850110496690f00d
Anna Steinhiler <annaSteinhilber@gmx.de>
2021-06-29 00:31:37 +0200

    Create model package

--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -3,7 +3,7 @@ package com.udacity.asteroidradar.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.udacity.asteroidradar.Asteroid
+import com.udacity.asteroidradar.model.Asteroid
 
 class MainViewModel : ViewModel() {
 
