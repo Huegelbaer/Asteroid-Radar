ed7a551324e675a8088f9bd4d994b0cd9de3a2a8
Anna Steinhiler <annaSteinhilber@gmx.de>
2021-06-30 12:16:39 +0200

    Use baseUrl defined in Constants

--- starter/app/src/main/java/com/udacity/asteroidradar/api/NasaApiService.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/api/NasaApiService.kt
@@ -3,14 +3,12 @@ package com.udacity.asteroidradar.api
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.udacity.asteroidradar.BuildConfig
+import com.udacity.asteroidradar.Constants
 import com.udacity.asteroidradar.model.PictureOfDay
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
-import retrofit2.http.Query
-
-private const val BASE_URL = "https://api.nasa.gov/"
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
@@ -18,7 +16,7 @@ private val moshi = Moshi.Builder()
 
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
-    .baseUrl(BASE_URL)
+    .baseUrl(Constants.BASE_URL)
     .build()
 
 interface NasaApiService {
