24b5d466a0aad31179a72cb858c68f348a05cfef
Anna Steinhiler <annaSteinhilber@gmx.de>
2021-07-06 10:53:01 +0200

    Add unit test for delete old asteroids (fails)

--- starter/app/src/androidTest/java/com/udacity/asteroidradar/DatabaseUnitTest.kt
+++ starter/app/src/androidTest/java/com/udacity/asteroidradar/DatabaseUnitTest.kt
@@ -6,7 +6,6 @@ import androidx.test.platform.app.InstrumentationRegistry
 import com.udacity.asteroidradar.database.AsteroidDAO
 import com.udacity.asteroidradar.database.AsteroidsDatabase
 import com.udacity.asteroidradar.database.DatabaseAsteroid
-import com.udacity.asteroidradar.model.Asteroid
 import org.junit.After
 import org.junit.Assert
 import org.junit.Before
@@ -44,7 +43,7 @@ class DatabaseUnitTest {
         asteroidDao.insert(asteroid)
 
         var asteroids = asteroidDao.getAll()
-        var savedAsteroid = asteroids.first()
+        var savedAsteroid = asteroids.value?.first()
         Assert.assertEquals(asteroid, savedAsteroid)
 
         val asteroid2 = DatabaseAsteroid(1, "1", "2021-01-03",
@@ -52,11 +51,51 @@ class DatabaseUnitTest {
             false)
         asteroidDao.insert(asteroid2)
 
-        Assert.assertEquals(1.2, savedAsteroid.relativeVelocity, 0.0)
+        Assert.assertEquals(1.2, savedAsteroid!!.relativeVelocity, 0.0)
 
         asteroids = asteroidDao.getAll()
-        savedAsteroid = asteroids.first()
+        savedAsteroid = asteroids.value?.first()
 
-        Assert.assertEquals(2.6, savedAsteroid.relativeVelocity, 0.0)
+        Assert.assertEquals(2.6, savedAsteroid!!.relativeVelocity, 0.0)
+    }
+
+    @Test
+    fun deleteOldAsteroids() {
+        val asteroids: ArrayList<DatabaseAsteroid> = arrayListOf(
+            DatabaseAsteroid(
+                3, "30.06", "2021-06-30",
+                0.0, 0.2, 1.2, 1.4,
+                false
+            ),
+            DatabaseAsteroid(
+                4, "01.07", "2021-07-01",
+                0.0, 0.2, 1.2, 1.4,
+                false
+            ),
+            DatabaseAsteroid(
+                5, "02.07", "2021-07-02",
+                0.0, 0.2, 1.2, 1.4,
+                false
+            ),
+            DatabaseAsteroid(
+                6, "03.07", "2021-07-03",
+                0.0, 0.2, 1.2, 1.4,
+                false
+            ),
+            DatabaseAsteroid(
+                2, "01.06", "2021-06-01",
+                0.0, 0.2, 1.2, 1.4,
+                false
+            )
+        )
+        asteroidDao.insertAll(asteroids)
+
+        var savedAsteroids = asteroidDao.getAll()
+        Assert.assertEquals(5, savedAsteroids.value?.count())
+
+        asteroidDao.deleteAsteroidsBeforeDate("2021-07-02")
+
+        savedAsteroids = asteroidDao.getAll()
+        Assert.assertEquals(2, savedAsteroids.value?.count())
     }
 }
